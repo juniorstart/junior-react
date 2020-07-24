2344813f5b89dde39f7151564b52f823ef82e186
Michalphs <michalphs@gmail.com>
2020-07-24 21:46:49 +0200

    Create api routes and add axios and lodash

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // application
 export const ROUTING_PREFIX = process.env.REACT_APP_ROUTING_PREFIX;
-
+export const API_URL = process.env.REACT_APP_API_URL;
 // node
 export const { NODE_ENV } = process.env;
