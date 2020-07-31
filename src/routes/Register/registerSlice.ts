45df308c407909d4f5571925b5eaa6be37a132a8
Michalphs <michalphs@gmail.com>
2020-07-31 07:33:55 +0200

    Delete check status from if

--- src/routes/Register/registerSlice.ts
+++ src/routes/Register/registerSlice.ts
@@ -51,7 +51,7 @@ export const registerUser = (user: User): AppThunk => async (dispatch: Dispatch)
     await api.register(user);
     dispatch(registerUserSuccess());
   } catch (err) {
-    if (err.response.status === 500 && err?.response?.data?.ErrorMessage) {
+    if (err?.response?.data?.ErrorMessage) {
       dispatch(loginError(err.response.data.ErrorMessage));
     }
     dispatch(registerUserError(err.toString()));
